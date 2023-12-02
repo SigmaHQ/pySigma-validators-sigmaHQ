1ea9e6c4050f8846ffebd7eb7e9cd7eda4f3f9d9
frack113 <62423083+frack113@users.noreply.github.com>
2023-12-02 08:32:54 +0100

    Add more validator (#2)

--- sigma/validators/sigmahq/metadata.py
+++ sigma/validators/sigmahq/metadata.py
@@ -14,25 +14,6 @@ from sigma.validators.base import (
 
 
 @dataclass
-class SigmahqFilenameIssue(SigmaValidationIssue):
-    description: ClassVar[str] = "Rule filemane doesn't match SigmaHQ standard"
-    severity: ClassVar[SigmaValidationIssueSeverity] = SigmaValidationIssueSeverity.HIGH
-    filename: str
-
-
-class SigmahqFilenameValidator(SigmaRuleValidator):
-    """Check rule filename match SigmaHQ standard."""
-
-    def validate(self, rule: SigmaRule) -> List[SigmaValidationIssue]:
-        filename_pattern = re.compile(r"[a-z0-9_]{10,90}\.yml")
-        if rule.source is not None:
-            filename = rule.source.path.name
-            if filename_pattern.match(filename) is None or not "_" in filename:
-                return [SigmahqFilenameIssue(rule, filename)]
-        return []
-
-
-@dataclass
 class SigmahqTitleLengthIssue(SigmaValidationIssue):
     description = "Rule has a title longer than 110 characters"
     severity = SigmaValidationIssueSeverity.MEDIUM
@@ -44,5 +25,117 @@ class SigmahqTitleLengthValidator(SigmaRuleValidator):
     def validate(self, rule: SigmaRule) -> List[SigmaValidationIssue]:
         if len(rule.title) > 110:
             return [SigmahqTitleLengthIssue([rule])]
+        else:
+            return []
+
+
+@dataclass
+class SigmahqStatusExistenceIssue(SigmaValidationIssue):
+    description = "Rule has no status"
+    severity = SigmaValidationIssueSeverity.MEDIUM
+
+
+class SigmahqStatusExistenceValidator(SigmaRuleValidator):
+    """Checks if rule has a status."""
+
+    def validate(self, rule: SigmaRule) -> List[SigmaValidationIssue]:
+        if rule.status is None:
+            return [SigmahqStatusExistenceIssue([rule])]
+        else:
+            return []
+
+
+@dataclass
+class SigmahqStatusUnsupportedIssue(SigmaValidationIssue):
+    description = "Rule has UNSUPPORTED status"
+    severity = SigmaValidationIssueSeverity.HIGH
+
+
+class SigmahqStatusUnsupportedValidator(SigmaRuleValidator):
+    """Checks if rule has a status UNSUPPORTED."""
+
+    def validate(self, rule: SigmaRule) -> List[SigmaValidationIssue]:
+        if rule.status and rule.status.name == "UNSUPPORTED":
+            return [SigmahqStatusUnsupportedIssue([rule])]
+        else:
+            return []
+
+
+@dataclass
+class SigmahqStatusDeprecatedIssue(SigmaValidationIssue):
+    description = "Rule has DEPRECATED status"
+    severity = SigmaValidationIssueSeverity.HIGH
+
+
+class SigmahqStatusDeprecatedValidator(SigmaRuleValidator):
+    """Checks if rule has a status DEPRECATED."""
+
+    def validate(self, rule: SigmaRule) -> List[SigmaValidationIssue]:
+        if rule.status and rule.status.name == "DEPRECATED":
+            return [SigmahqStatusDeprecatedIssue([rule])]
+        else:
+            return []
+
+
+@dataclass
+class SigmahqDateExistenceIssue(SigmaValidationIssue):
+    description = "Rule has no date"
+    severity = SigmaValidationIssueSeverity.MEDIUM
+
+
+class SigmahqDateExistenceValidator(SigmaRuleValidator):
+    """Checks if rule has a data."""
+
+    def validate(self, rule: SigmaRule) -> List[SigmaValidationIssue]:
+        if rule.date is None:
+            return [SigmahqDateExistenceIssue([rule])]
+        else:
+            return []
+
+
+@dataclass
+class SigmahqDescriptionExistenceIssue(SigmaValidationIssue):
+    description = "Rule has no description"
+    severity = SigmaValidationIssueSeverity.MEDIUM
+
+
+class SigmahqDescriptionExistenceValidator(SigmaRuleValidator):
+    """Checks if rule has a description."""
+
+    def validate(self, rule: SigmaRule) -> List[SigmaValidationIssue]:
+        if rule.description is None:
+            return [SigmahqDescriptionExistenceIssue([rule])]
+        else:
+            return []
+
+
+@dataclass
+class SigmahqDescriptionLengthIssue(SigmaValidationIssue):
+    description = "Rule has a too short description"
+    severity = SigmaValidationIssueSeverity.MEDIUM
+
+
+class SigmahqDescriptionLengthValidator(SigmaRuleValidator):
+    """Checks if rule has a description."""
+
+    def validate(self, rule: SigmaRule) -> List[SigmaValidationIssue]:
+        if rule.description is not None and len(rule.description) < 16:
+            return [SigmahqDescriptionLengthIssue([rule])]
+        else:
+            return []
+
+
+@dataclass
+class SigmahqLevelExistenceIssue(SigmaValidationIssue):
+    description = "Rule has no level"
+    severity = SigmaValidationIssueSeverity.MEDIUM
+
+
+class SigmahqLevelExistenceValidator(SigmaRuleValidator):
+    """Checks if rule has a level."""
+
+    def validate(self, rule: SigmaRule) -> List[SigmaValidationIssue]:
+        if rule.level is None:
+            return [SigmahqLevelExistenceIssue([rule])]
         else:
             return []
--- tests/test_validators.py
+++ tests/test_validators.py
@@ -2,15 +2,33 @@ from uuid import UUID
 from wsgiref.validate import validator
 
 import pytest
-from sigma.rule import SigmaRule
-from sigma.types import SigmaString
+from sigma.rule import SigmaRule, SigmaLogSource
 from sigma.collection import SigmaCollection
 
-from sigma.validators.sigmahq.metadata import (
+from sigma.validators.sigmahq.filename import (
     SigmahqFilenameIssue,
     SigmahqFilenameValidator,
+    SigmahqFilenamePrefixIssue,
+    SigmahqFilenamePrefixValidator,
+)
+
+from sigma.validators.sigmahq.metadata import (
     SigmahqTitleLengthIssue,
     SigmahqTitleLengthValidator,
+    SigmahqStatusExistenceIssue,
+    SigmahqStatusExistenceValidator,
+    SigmahqStatusUnsupportedIssue,
+    SigmahqStatusUnsupportedValidator,
+    SigmahqStatusDeprecatedIssue,
+    SigmahqStatusDeprecatedValidator,
+    SigmahqDateExistenceIssue,
+    SigmahqDateExistenceValidator,
+    SigmahqDescriptionExistenceIssue,
+    SigmahqDescriptionExistenceValidator,
+    SigmahqDescriptionLengthIssue,
+    SigmahqDescriptionLengthValidator,
+    SigmahqLevelExistenceIssue,
+    SigmahqLevelExistenceValidator,
 )
 
 
@@ -21,6 +39,19 @@ def test_validator_SigmahqFilename():
     )
     rule = sigma_collection[0]
     assert validator.validate(rule) == [SigmahqFilenameIssue([rule], "Name.yml")]
+
+
+def test_validator_SigmahqPrefixFilename():
+    validator = SigmahqFilenamePrefixValidator()
+    sigma_collection = SigmaCollection.load_ruleset(
+        ["tests/files/rule_filename_errors"]
+    )
+    rule = sigma_collection[0]
+    assert validator.validate(rule) == [
+        SigmahqFilenamePrefixIssue(
+            [rule], "Name.yml", SigmaLogSource("process_creation", "windows", None)
+        )
+    ]
 
 
 def test_validator_SigmahqTitleLength():
@@ -38,3 +69,119 @@ def test_validator_SigmahqTitleLength():
     """
     )
     assert validator.validate(rule) == [SigmahqTitleLengthIssue([rule])]
+
+
+def test_validator_SigmahqStatusUnsupported():
+    validator = SigmahqStatusUnsupportedValidator()
+    rule = SigmaRule.from_yaml(
+        """
+    title: test
+    status: unsupported
+    logsource:
+        category: test
+    detection:
+        sel:
+            field: path\\*something
+        condition: sel
+    """
+    )
+    assert validator.validate(rule) == [SigmahqStatusUnsupportedIssue([rule])]
+
+
+def test_validator_SigmahqStatusDeprecated():
+    validator = SigmahqStatusDeprecatedValidator()
+    rule = SigmaRule.from_yaml(
+        """
+    title: test
+    status: deprecated
+    logsource:
+        category: test
+    detection:
+        sel:
+            field: path\\*something
+        condition: sel
+    """
+    )
+    assert validator.validate(rule) == [SigmahqStatusDeprecatedIssue([rule])]
+
+
+def test_validator_SigmahqDateExistence():
+    validator = SigmahqDateExistenceValidator()
+    rule = SigmaRule.from_yaml(
+        """
+    title: test
+    status: stable
+    logsource:
+        category: test
+    detection:
+        sel:
+            field: path\\*something
+        condition: sel
+    """
+    )
+    assert validator.validate(rule) == [SigmahqDateExistenceIssue([rule])]
+
+
+def test_validator_SigmahqStatusExistence():
+    validator = SigmahqStatusExistenceValidator()
+    rule = SigmaRule.from_yaml(
+        """
+    title: Test
+    logsource:
+        category: test
+    detection:
+        sel:
+            field: value
+        condition: sel
+    """
+    )
+    assert validator.validate(rule) == [SigmahqStatusExistenceIssue([rule])]
+
+
+def test_validator_SigmahqDescriptionExistence():
+    validator = SigmahqDescriptionExistenceValidator()
+    rule = SigmaRule.from_yaml(
+        """
+    title: Test
+    logsource:
+        category: test
+    detection:
+        sel:
+            field: value
+        condition: sel
+    """
+    )
+    assert validator.validate(rule) == [SigmahqDescriptionExistenceIssue([rule])]
+
+
+def test_validator_SigmahqDescriptionLength():
+    validator = SigmahqDescriptionLengthValidator()
+    rule = SigmaRule.from_yaml(
+        """
+    title: Test
+    description: Test
+    logsource:
+        category: test
+    detection:
+        sel:
+            field: value
+        condition: sel
+    """
+    )
+    assert validator.validate(rule) == [SigmahqDescriptionLengthIssue([rule])]
+
+
+def test_validator_SigmahqLevelExistence():
+    validator = SigmahqLevelExistenceValidator()
+    rule = SigmaRule.from_yaml(
+        """
+    title: Test
+    logsource:
+        category: test
+    detection:
+        sel:
+            field: value
+        condition: sel
+    """
+    )
+    assert validator.validate(rule) == [SigmahqLevelExistenceIssue([rule])]
