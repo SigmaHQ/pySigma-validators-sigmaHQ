b17cc950feaecf75afc1b568594f0038bd78853d
frack113 <62423083+frack113@users.noreply.github.com>
2024-11-10 16:41:01 +0100

    Add remote url

--- sigma/validators/sigmahq/config.py
+++ sigma/validators/sigmahq/config.py
@@ -4,14 +4,18 @@ from typing import Dict, List, Tuple
 from sigma.rule import SigmaLogSource
 import json
 import os
+import pathlib
+import requests
 
 
 def load_remote_json(url: str, filename: str) -> dict:
-    # url to check
-    # else
     full_name = os.getcwd() + "/validator_json/" + filename
-    with open(full_name, "r", encoding="UTF-8") as file:
-        json_dict = json.load(file)
+    if pathlib.Path(full_name).exists():
+        with open(full_name, "r", encoding="UTF-8") as file:
+            json_dict = json.load(file)
+    else:
+        r = requests.get(url)
+        json_dict = json.loads(r.content.decode(encoding="UTF-8"))
     return json_dict
 
 
@@ -21,8 +25,8 @@ def core_logsource(source: SigmaLogSource) -> SigmaLogSource:
     )
 
 
-def load_taxonomy_json(json_name: str) -> dict:
-    json_dict = load_remote_json("github", json_name)
+def load_taxonomy_json(url: str, json_name: str) -> dict:
+    json_dict = load_remote_json(url, json_name)
     info = {}
     for value in json_dict["taxonomy"].values():
         logsource = core_logsource(SigmaLogSource.from_dict(value["logsource"]))
@@ -38,17 +42,17 @@ def get_taxonomy_field(sigma: dict) -> dict:
     return field_info
 
 
-def load_filepattern_json(json_name):
+def load_filepattern_json(url: str, json_name: str):
     prefix_info = {}
-    json_dict = load_remote_json("github", json_name)
+    json_dict = load_remote_json(url, json_name)
     for value in json_dict["pattern"].values():
         logsource = core_logsource(SigmaLogSource.from_dict(value["logsource"]))
         prefix_info[logsource] = value["prefix"]
     return prefix_info
 
 
-def load_windows_json(json_name):
-    json_dict = load_remote_json("github", json_name)
+def load_windows_json(url: str, json_name: str):
+    json_dict = load_remote_json(url, json_name)
     data = dict()
     for category in json_dict["category_provider_name"]:
         data[SigmaLogSource(product="windows", category=category, service=None)] = (
@@ -68,15 +72,20 @@ class ConfigHQ:
     windows_provider_name: Dict[SigmaLogSource, List[str]] = {}
 
     def __init__(self) -> None:
-        self.sigma_taxonomy = load_taxonomy_json("sigma.json")
+        self.sigma_taxonomy = load_taxonomy_json(
+            url="https://github.com/SigmaHQ/pySigma-validators-sigmaHQ/raw/refs/heads/main/validator_json/sigma.json",
+            json_name="sigma.json",
+        )
         self.sigma_fieldsname = get_taxonomy_field(self.sigma_taxonomy)
         self.sigma_fieldsname_unicast = {
             k: [v.lower() for v in l] for k, l in self.sigma_fieldsname.items()
         }
 
         self.sigmahq_logsource_filepattern = load_filepattern_json(
-            "sigmahq_filename.json"
+            url="https://github.com/SigmaHQ/pySigma-validators-sigmaHQ/raw/refs/heads/main/validator_json/sigmahq_filename.json",
+            json_name="sigmahq_filename.json",
         )
         self.windows_no_eventid, self.windows_provider_name = load_windows_json(
-            "sigmahq_windows_validator.json"
+            url="https://github.com/SigmaHQ/pySigma-validators-sigmaHQ/raw/refs/heads/main/validator_json/sigmahq_windows_validator.json",
+            json_name="sigmahq_windows_validator.json",
         )
