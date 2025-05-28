994c34529d38f832dbc0a7f81a60a7dbe8fafe32
frack113 <62423083+frack113@users.noreply.github.com>
2025-05-28 17:30:55 +0200

    refactor: 🔨 Sort logsource key

--- tools/update_ref.py
+++ tools/update_ref.py
@@ -1,6 +1,6 @@
-# Version 0.0.1
+# Version 0.1.0
 # Author: frack113
-# Date: 2025/04/25
+# Date: 2025/05/28
 
 import json
 from sys import stderr, stdout
@@ -12,42 +12,64 @@ def core_logsource(source: SigmaLogSource) -> SigmaLogSource:
     return SigmaLogSource(product=source.product, category=source.category, service=source.service)
 
 
-# Filename
+def key_logsource(source: dict) -> str:
+    product = source["product"] if source["product"] else "none"
+    category = source["category"] if source["category"] else "none"
+    service = source["service"] if source["service"] else "none"
+    return f"{product}_{category}_{service}"
+
+
+#
+# sigmahq_filename.json
+#
 with open("tools/sigmahq_filename.json", "r", encoding="UTF-8") as file:
     json_dict = json.load(file)
 
-filename_version = json_dict["version"]
-filename_info = {}
-for value in json_dict["pattern"].values():
-    logsource = core_logsource(SigmaLogSource.from_dict(value["logsource"]))
-    filename_info[logsource] = value["prefix"]
+    filename_version = json_dict["version"]
+    filename_info = dict()
 
+    temp = {key_logsource(v["logsource"]): v for v in json_dict["pattern"].values()}
+    for key in sorted(temp.keys(), key=str.casefold):
+        value = temp[key]
+        logsource = core_logsource(SigmaLogSource.from_dict(value["logsource"]))
+        filename_info[logsource] = value["prefix"]
+
+#
+# sigma.json
+#
 with open("tools/sigma.json", "r", encoding="UTF-8") as file:
     json_dict = json.load(file)
 
-taxonomy_version = json_dict["version"]
-taxonomy_info = {}
-taxonomy_definition = {}
-for value in json_dict["taxonomy"].values():
-    logsource = core_logsource(SigmaLogSource.from_dict(value["logsource"]))
-    fieldlist = []
-    fieldlist.extend(value["field"]["native"])
-    fieldlist.extend(value["field"]["custom"])
-    taxonomy_info[logsource] = sorted(fieldlist, key=str.casefold)
-    taxonomy_definition[logsource] = value["logsource"]["definition"]
+    taxonomy_version = json_dict["version"]
+    taxonomy_info = dict()
+    taxonomy_definition = dict()
 
-taxonomy_info_unicast = {k: [v.lower() for v in l] for k, l in taxonomy_info.items()}
+    temp = {key_logsource(v["logsource"]): v for v in json_dict["taxonomy"].values()}
+    for key in sorted(temp.keys(), key=str.casefold):
+        value = temp[key]
+        logsource = core_logsource(SigmaLogSource.from_dict(value["logsource"]))
+        fieldlist = []
+        fieldlist.extend(value["field"]["native"])
+        fieldlist.extend(value["field"]["custom"])
+        taxonomy_info[logsource] = sorted(fieldlist, key=str.casefold)
+        taxonomy_definition[logsource] = value["logsource"]["definition"]
 
+    taxonomy_info_unicast = {k: [v.lower() for v in l] for k, l in taxonomy_info.items()}
+
+#
+# sigmahq_windows_validator.json
+#
 with open("tools/sigmahq_windows_validator.json", "r", encoding="UTF-8") as file:
     json_dict = json.load(file)
 
-windows_version = json_dict["version"]
-windows_provider_name = dict()
-for category in json_dict["category_provider_name"]:
-    windows_provider_name[SigmaLogSource(product="windows", category=category, service=None)] = (
-        json_dict["category_provider_name"][category]
-    )
-windows_no_eventid = json_dict["category_no_eventid"]
+    windows_version = json_dict["version"]
+    windows_provider_name = dict()
+
+    for category in sorted(json_dict["category_provider_name"], key=str.casefold):
+        windows_provider_name[
+            SigmaLogSource(product="windows", category=category, service=None)
+        ] = json_dict["category_provider_name"][category]
+    windows_no_eventid = sorted(json_dict["category_no_eventid"], key=str.casefold)
 
 
 # python data
@@ -57,33 +79,33 @@ with open("sigma/validators/sigmahq/sigmahq_data.py", "wt", encoding="utf-8", ne
     print(f'\nfile_pattern_version: str = "{filename_version}"', file=file)
     print(
         "ref_sigmahq_logsource_filepattern: Dict[SigmaLogSource, str] = "
-        + pformat(filename_info, indent=4, sort_dicts=True),
+        + pformat(filename_info, indent=4, sort_dicts=False),
         file=file,
     )
     print(f'\ntaxonomy_version: str = "{taxonomy_version}"', file=file)
     print(
         "ref_sigmahq_fieldsname: Dict[SigmaLogSource, List[str]] = "
-        + pformat(taxonomy_info, indent=4, sort_dicts=True),
+        + pformat(taxonomy_info, indent=4, sort_dicts=False),
         file=file,
     )
     print(
         "ref_sigmahq_fieldsname_unicast: Dict[SigmaLogSource, List[str]] = "
-        + pformat(taxonomy_info_unicast, indent=4, sort_dicts=True),
+        + pformat(taxonomy_info_unicast, indent=4, sort_dicts=False),
         file=file,
     )
     print(
         "ref_sigmahq_logsource_definition: Dict[SigmaLogSource, str] = "
-        + pformat(taxonomy_definition, indent=4, sort_dicts=True, width=200),
+        + pformat(taxonomy_definition, indent=4, sort_dicts=False, width=200),
         file=file,
     )
     print(f'\nwindows_version: str = "{windows_version}"', file=file)
     print(
         "ref_windows_provider_name: Dict[SigmaLogSource, List[str]] = "
-        + pformat(windows_provider_name, indent=4, sort_dicts=True),
+        + pformat(windows_provider_name, indent=4, sort_dicts=False),
         file=file,
     )
     print(
         "ref_windows_no_eventid: List[str] = "
-        + pformat(windows_no_eventid, indent=4, sort_dicts=True),
+        + pformat(windows_no_eventid, indent=4, sort_dicts=False),
         file=file,
     )
