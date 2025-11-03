d88519cd37db4e9982d97d53d6165285bb0e8518
swachchhanda000 <87493836+swachchhanda000@users.noreply.github.com>
2025-11-03 13:30:10 +0545

    feat: add validator for modified date order

--- sigma/validators/sigmahq/metadata.py
+++ sigma/validators/sigmahq/metadata.py
@@ -76,6 +76,34 @@ class SigmahqDateExistenceValidator(SigmaRuleValidator):
         else:
             return []
 
+@dataclass
+class SigmahqModifiedDateOrderIssue(SigmaValidationIssue):
+    description: ClassVar[str] = "Rule has a modified date that is same or older than the date field"
+    severity: ClassVar[SigmaValidationIssueSeverity] = SigmaValidationIssueSeverity.HIGH
+
+
+class SigmahqModifiedDateOrderValidator(SigmaRuleValidator):
+    """Checks if a rule has a modified date that is same or older than the date field."""
+
+    def validate(self, rule: SigmaRuleBase) -> List[SigmaValidationIssue]:
+        if rule.date is not None and rule.modified is not None:
+            if rule.modified <= rule.date:
+                return [SigmahqModifiedDateOrderIssue([rule])]
+        return []
+
+@dataclass
+class SigmahqModifiedWithoutDateIssue(SigmaValidationIssue):
+    description: ClassVar[str] = "Rule has a modified field without a date field"
+    severity: ClassVar[SigmaValidationIssueSeverity] = SigmaValidationIssueSeverity.HIGH
+    
+class SigmahqModifiedWithoutDateValidator(SigmaRuleValidator):
+    """Checks if a rule has a modified field without a date field."""
+
+    def validate(self, rule: SigmaRuleBase) -> List[SigmaValidationIssue]:
+        if rule.modified is not None and rule.date is None:
+            return [SigmahqModifiedWithoutDateIssue([rule])]
+        return []
+
 
 @dataclass
 class SigmahqDescriptionExistenceIssue(SigmaValidationIssue):
