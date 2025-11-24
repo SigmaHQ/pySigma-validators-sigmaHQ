8579290cc8a01a780ca0b63e92792443987ccf4e
frack113 <62423083+frack113@users.noreply.github.com>
2025-11-24 16:19:35 +0100

    feat: ✨ Add SigmahqFieldsExistenceValidator

--- sigma/validators/sigmahq/metadata.py
+++ sigma/validators/sigmahq/metadata.py
@@ -79,6 +79,22 @@ class SigmahqDateExistenceValidator(SigmaRuleValidator):
 
 
 @dataclass
+class SigmahqFieldsExistenceIssue(SigmaValidationIssue):
+    description: ClassVar[str] = "Rule is using the deprecated fields"
+    severity: ClassVar[SigmaValidationIssueSeverity] = SigmaValidationIssueSeverity.MEDIUM
+
+
+class SigmahqFieldsExistenceValidator(SigmaRuleValidator):
+    """Checks if a rule is musing the deprecated fields."""
+
+    def validate(self, rule: SigmaRule | SigmaCorrelationRule) -> List[SigmaValidationIssue]:
+        if len(rule.fields) > 0:
+            return [SigmahqFieldsExistenceIssue([rule])]
+        else:
+            return []
+
+
+@dataclass
 class SigmahqModifiedDateOrderIssue(SigmaValidationIssue):
     description: ClassVar[str] = (
         "Rule has a modified field whose value is older than that of the date field. The modified date has always to be newer than date."
